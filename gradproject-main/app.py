8c8527520997ff4801790e385a4501239f3b339d
berkay <berkayd2000@gmail.com>
2024-01-11 02:02:33 +0300

    trying something to mongodb py and react

--- gradproject-main/app.py
+++ gradproject-main/app.py
@@ -1,9 +1,40 @@
-from PIL import Image
+import os
 import pytesseract
+import cv2
+import re
+import spacy
+from PIL import Image
+from pytesseract import Output
+from spacy import displacy
+from pymongo import MongoClient
 
-print(pytesseract.image_to_string(Image.open('test1.jpg'),lang='tur'))
-print(pytesseract.image_to_string(Image.open('test.jpg'),lang='tur'))
+image_dir='gradproject-main\\'
+images = [file for file in os.listdir(image_dir) if file.lower().endswith(".jpg")]
+#img = cv2.imread('gradproject-main\\gradproject-main\\test.jpg')
 
-print(pytesseract.image_to_string(Image.open('test1.jpg'),lang='tur'))
-print(pytesseract.image_to_string(Image.open('test.jpg'),lang='tur'))
 
+#custom_config = r'--oem 3 --psm 6 -l tur'
+for image_name in images:
+    image_path = os.path.join(image_dir, image_name)
+    
+    # Görüntüyü oku
+    img = cv2.imread(image_path)
+    
+    # OCR işlemi uygula
+    text = pytesseract.image_to_string(img,lang='tur')
+    print(f"File: {image_name}")
+    print(text)
+    print("--------------------------")
+
+#text = pytesseract.image_to_string(img, config=custom_config)
+
+
+#date_pattern = r'(\d{1,2}/\d{1,2}/\d{4})'
+#date = re.findall(date_pattern, text)
+
+#time_pattern = r'(\d{1,2}:\d{2})'
+#time = re.findall(time_pattern, text)
+#print(text)
+#print("--------------------------")
+#print("Date: ", date)
+#print("Time: ", time)
