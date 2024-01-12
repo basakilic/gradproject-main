8337e4e78f5fd35c7f21f83594fccec8b1de26cc
basakilic <basakk244@gmail.com>
2024-01-12 03:59:03 +0300

    routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,27 @@
 import './App.css';
-function App() {
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Homepage from './homepage'
+import Login from './Login'
+import Signup from './Signup'
+import ForgotPassword from './ForgotPassword'
+import Profile from './Profile'
+import EditProfile from './EditProfile'
 
-}
+
+function App() {
+    return (
+        <BrowserRouter>
+          <Routes>
+          <Route path='/' element={<Homepage />}></Route>
+          <Route path='/homepage' element={<Homepage />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
+          <Route path='/forgotpassword' element={<ForgotPassword />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
+          <Route path='/editprofile' element={<EditProfile />}></Route>
+
+          </Routes>
+        </BrowserRouter>)
+    }
 export default App
