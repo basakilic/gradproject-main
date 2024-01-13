566be1bccd1f18b234e1f391f17e55fd511828d3
berkay <berkayd2000@gmail.com>
2024-01-13 15:43:21 +0300

    receipts page modified and fixed.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import ListReceiptsComponent from './components/ListReceiptsComponent.jsx';
+import HeaderComponent from './components/HeaderComponent';
 import Homepage from './Homepage'
 import Login from './Login'
 import Signup from './Signup'
 import ForgotPassword from './ForgotPassword'
 import Profile from './Profile'
 import EditProfile from './EditProfile'
-import ListReceiptsComponent from './components/ListReceiptsComponent';
-import HeaderComponent from './components/HeaderComponent';
+
 //import FooterComponent from './components/FooterComponent';
 //import CreateReceiptComponent from './components/CreateReceiptComponent';
 //import UpdateReceiptComponent from './components/UpdateReceiptComponent';
@@ -17,28 +18,30 @@ import HeaderComponent from './components/HeaderComponent';
 function App() {
   return (
     <div>
-        <Router>
-              <HeaderComponent />
-                <div className="container">
-                    <Routes> 
-                          {/* <Route path = "/" exact component = {ListReceiptsComponent}></Route> */}
-                          <Route path = "/receipts" component = {ListReceiptsComponent}></Route>
-                          {/* <Route path = "/add-receipt/:id" component = {CreateReceiptComponent}></Route>
-                          <Route path = "/view-receipt/:id" component = {ViewReceiptComponent}></Route> */}
-                          {/* <Route path = "/update-receipt/:id" component = {UpdateReceiptComponent}></Route> */}
-                          <Route path='/' element={<Homepage />}></Route>
-                          <Route path='/homepage' element={<Homepage />}></Route>
-                          <Route path='/login' element={<Login />}></Route>
-                          <Route path='/signup' element={<Signup />}></Route>
-                          <Route path='/forgotpassword' element={<ForgotPassword />}></Route>
-                          <Route path='/profile' element={<Profile />}></Route>
-                          <Route path='/editprofile' element={<Profile />}></Route>
-                    </Routes>
-                </div>
-              {/* <FooterComponent /> */}
-        </Router>
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Routes>
+          {/* <Route path = "/" exact component = {ListReceiptsComponent}></Route> */}
+        
+          {/* <Route path = "/add-receipt/:id" component = {CreateReceiptComponent}></Route>
+          <Route path = "/view-receipt/:id" component = {ViewReceiptComponent}></Route> */}
+          {/* <Route path = "/update-receipt/:id" component = {UpdateReceiptComponent}></Route> */}
+          <Route path="/receipts" Component={ListReceiptsComponent}></Route>
+          <Route path='/' element={<Homepage />}></Route>
+          <Route path='/homepage' element={<Homepage />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
+          <Route path='/forgotpassword' element={<ForgotPassword />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
+          <Route path='/editprofile' element={<Profile />}></Route>
+          </Routes>
+          
+        </div>
+        {/* <FooterComponent /> */}
+      </Router>
     </div>
-    
+
   );
 }
 export default App
