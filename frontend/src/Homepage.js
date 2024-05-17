47adca9274b579c3c98ce69c54141bcebd02873c
basakilic <basakk244@gmail.com>
2024-05-17 04:13:04 +0300

    homepage updated

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
 import Navbar from './Navbar';
 import './Homepage.css';
 import backgroundImage from './background.jpg';
 
 function Homepage() {
-
+  const name = localStorage.getItem('name');
+  const time = localStorage.getItem('time');
+  const quota = localStorage.getItem('quota');
+  const location = localStorage.getItem('location');
+  const date = localStorage.getItem('date');
+  const description = localStorage.getItem('description');
+  const organizer = localStorage.getItem('organizer');
 
   return (
 
@@ -20,10 +25,10 @@ function Homepage() {
 
         <div>
           <h1 style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: '#fff', fontSize: '48px', fontWeight: 'bold', textAlign: 'center' }}>
-            Welcome to ScanRes !
+            Welcome to FUNHOUR !
           </h1>
           <p style={{ position: 'absolute', top: '65%', left: '50%', transform: 'translate(-50%, -50%)', color: '#fff', fontSize: '28px', fontWeight: 'lighter', fontFamily: 'sans-serif', textAlign: 'center' }}>
-            Thanks to the facilities provided by ScanRes, you will be able to easily process and keep records of your receipts.
+            ENROLL WORKSHOPS OR CREATE WORKSHOPS !
           </p>
         </div>
       </div>
